585732f8604ed4af6c38925cb5e2f5e85577ea23
galcyurio <galcyurio@gmail.com>
2018-08-18 11:38:12 +0900

    re #2 버튼 클릭시 현재 lifecycle state 보여주기

--- LifecycleToastSample/app/src/main/java/com/github/galcyurio/lifecycletoastsample/MainActivity.kt
+++ LifecycleToastSample/app/src/main/java/com/github/galcyurio/lifecycletoastsample/MainActivity.kt
@@ -3,12 +3,15 @@ package com.github.galcyurio.lifecycletoastsample
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLifecycle.setOnClickListener { showCurrentLifecycle() }
     }
 
     fun showCurrentLifecycle() {
