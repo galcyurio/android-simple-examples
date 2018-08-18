0ea6271e0a00d48130f425036c508614b38f8dfe
galcyurio <galcyurio@gmail.com>
2018-08-18 19:31:35 +0900

    close #2 `MainPresenter`에 Lifecycle 이벤트 처리 함수 추가

--- LifecycleToastSample/app/src/main/java/com/github/galcyurio/lifecycletoastsample/MainActivity.kt
+++ LifecycleToastSample/app/src/main/java/com/github/galcyurio/lifecycletoastsample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.github.galcyurio.lifecycletoastsample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -18,6 +19,8 @@ class MainActivity : AppCompatActivity(), MainContract.View {
     }
 
     override fun showCurrentLifecycle() {
+        // lifecycle.currentState -> 신뢰할 수 없음
+        Log.i(MainActivity::class.java.name, lifecycle.currentState.name)
         Toast.makeText(this, lifecycle.currentState.name, Toast.LENGTH_SHORT).show()
     }
 }
