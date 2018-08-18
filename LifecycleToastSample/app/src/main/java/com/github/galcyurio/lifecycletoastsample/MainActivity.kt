51c84d456c84c946a77591bf094f27a2f9098118
galcyurio <galcyurio@gmail.com>
2018-08-18 11:20:56 +0900

    re #2 showCurrentLifecycle() 함수 추가

--- LifecycleToastSample/app/src/main/java/com/github/galcyurio/lifecycletoastsample/MainActivity.kt
+++ LifecycleToastSample/app/src/main/java/com/github/galcyurio/lifecycletoastsample/MainActivity.kt
@@ -2,11 +2,16 @@ package com.github.galcyurio.lifecycletoastsample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun showCurrentLifecycle() {
+        Toast.makeText(this, lifecycle.currentState.name, Toast.LENGTH_SHORT).show()
     }
 }
