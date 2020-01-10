fe9de8e5621d33d1dc8c80728e10f6ac1b6a49f7
galcyurio <galcyurio@gmail.com>
2020-01-10 15:54:23 +0900

    데이터베이스 버전 넘버 변경 (#6)

--- RoomTodo/app/src/main/java/com/github/galcyurio/roomtodo/data/AppDatabase.kt
+++ RoomTodo/app/src/main/java/com/github/galcyurio/roomtodo/data/AppDatabase.kt
@@ -6,7 +6,7 @@ import com.github.galcyurio.roomtodo.data.domain.Task
 
 @Database(
     entities = [Task::class],
-    version = 1)
+    version = 2)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
 }
