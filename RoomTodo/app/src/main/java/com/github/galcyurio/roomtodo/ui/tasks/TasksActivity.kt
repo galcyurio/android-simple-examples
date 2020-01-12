7375bfba5e8f06834d40a02b5894b2f87737391f
galcyurio <galcyurio@gmail.com>
2020-01-13 08:43:19 +0900

    TasksActivity에 데이터 바인딩 적용 (#6)

--- RoomTodo/app/src/main/java/com/github/galcyurio/roomtodo/ui/tasks/TasksActivity.kt
+++ RoomTodo/app/src/main/java/com/github/galcyurio/roomtodo/ui/tasks/TasksActivity.kt
@@ -3,32 +3,35 @@ package com.github.galcyurio.roomtodo.ui.tasks
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.github.galcyurio.roomtodo.R
+import com.github.galcyurio.roomtodo.databinding.ActivityTasksBinding
 import com.github.galcyurio.roomtodo.misc.TaskItemAdapter
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.activity_tasks.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class TasksActivity : AppCompatActivity() {
     private val viewModel: TasksViewModel by viewModel()
     private val disposable = CompositeDisposable()
+    private lateinit var binding: ActivityTasksBinding
+
     private val taskItemAdapter: TaskItemAdapter by lazy { TaskItemAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tasks)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_tasks)
 
-        recycler.adapter = taskItemAdapter
+        binding.recycler.adapter = taskItemAdapter
 
         viewModel.fetchTasks()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .doOnSubscribe { loading.show() }
-            .doFinally { loading.hide() }
+            .doOnSubscribe { binding.loading.show() }
+            .doFinally { binding.loading.hide() }
             .subscribeBy(
                 onSuccess = taskItemAdapter::addTasks,
                 onError = { error ->
