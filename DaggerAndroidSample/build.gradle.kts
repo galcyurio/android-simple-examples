b1cfefb525202644852fbf791d8bc3c982b88bb6
galcyurio <galcyurio@gmail.com>
2020-11-02 22:55:23 +0900

    DaggerAndroidSample 프로젝트 생성

---
+++ DaggerAndroidSample/build.gradle.kts
@@ -0,0 +1,23 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    val kotlinVersion by extra("1.4.10")
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
