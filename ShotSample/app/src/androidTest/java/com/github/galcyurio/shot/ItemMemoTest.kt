a2d762a050e63ff69cda61864c5fd5cb08de369c
galcyurio <galcyurio@gmail.com>
2021-11-18 01:02:29 +0900

    ItemMemoTest 추가

---
+++ ShotSample/app/src/androidTest/java/com/github/galcyurio/shot/ItemMemoTest.kt
@@ -0,0 +1,38 @@
+package com.github.galcyurio.shot
+
+import android.view.LayoutInflater
+import android.view.View
+import androidx.test.platform.app.InstrumentationRegistry
+import com.github.galcyurio.shot.databinding.ItemMemoBinding
+import com.karumi.shot.ScreenshotTest
+import org.junit.Test
+
+class ItemMemoTest : ScreenshotTest {
+    @Test
+    fun test1() {
+        compareScreenshot(createView())
+    }
+
+    @Test
+    fun test2() {
+        runOnUi {
+            val binding = createBinding()
+            binding.title = "foo"
+            binding.content = "bar"
+            compareScreenshot(binding.root)
+        }
+    }
+
+    //region Fixture
+    private fun createView(): View {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val inflater = LayoutInflater.from(context)
+        return inflater.inflate(R.layout.item_memo, null, false)
+    }
+
+    private fun createBinding(): ItemMemoBinding {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        return ItemMemoBinding.inflate(LayoutInflater.from(context))
+    }
+    //endregion
+}
